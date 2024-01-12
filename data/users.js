7b6cc7f4901f32db25217a609c1c39ec5523176d
pswanagan <pswanagan@gmail.com>
2024-01-12 11:20:11 -0600

    fixed errors and git configuration

--- data/users.js
+++ data/users.js
@@ -1,4 +1,4 @@
-/ Creating the simple data structures we'll work with.
+// Creating the simple data structures we'll work with.
 // How we choose to store and organize this data is very important!
 // Different options and techniques for storing data and
 // creating relationships between different data sets will be
